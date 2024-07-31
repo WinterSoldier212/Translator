297f05ccf2185d1847046e23b771d752f7e95d26
WinterSoldier212 <04.12.2006mmm@gmail.com>
2024-07-31 17:07:35 +0300

    Complete Lexer

--- Translator/src/lex/lexer.rs
+++ Translator/src/lex/lexer.rs
@@ -1,39 +1,48 @@
-
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub enum Token {
-    LP, RP, LC, RC, LB, RB,
-
+//  [a-zA-Z_][a-zA-Z0-9_]*
     Id {id: String},
+//  [0-9]*           [0-9]*.[0-9]*       '[\]+[a-zA-Z0-9]'* 
     IntLit {i: i32}, DoubleLit{ f: f64}, CharLit {c: u8}, 
-
-    // LongLit{l: i64}, FloatLit{ f: f32}, ShortLit {i: i16},
-
-    Dot, Semicolon, Comma, Ampersand,
-
-    Plus, Minus, Star, Divide, Mod,
+//  (   )   {   }   [   ]
+    LP, RP, LC, RC, LB, RB,
+//   .       ;        ,
+    Dot, Semicolon, Comma, 
+//    +     -     *     /    %
+    Plus, Minus, Star, Div, Mod,
+//      +=         -=        *=        /=         %=
+    PlusEqual, MinusEqual, StarEqual, DivEqual, ModEqual,
+//   ==      !=         >       <
     Equal, NotEqual, Bigger, Lesser,
+//      >=              <=
     BiggerOrEqual, LesserOrEqual,
-
-    Assign, 
+//     =
+    Assignment, 
+//  true  false
     True, False,
+//      ++         --
     Increment, Decrement,
-    
+//   &&  ||     !
     And, Or, Negation,
-
-    If, Else, For, While, 
+//       &          |
+    BitwiseAnd, BitwiseOr,
+//         &=               |=   
+    BitwiseAndEqual, BitwiseOrEqual,   
+//  if  else  for  while
+    If, Else, For, While,
+//  break  continue  return 
     Break, Continue, Return,
-    
-    // LongT, FloatT, ShortT,
+//  int   doble    char   void
     IntT, DoubleT, CharT, VoidT
 }
-
-
 
 pub fn lexical_analysis(s: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
     let mut chr = s.chars().peekable();
 
     while let Some(ch) = chr.next() {
-        if ch.is_whitespace() { continue; }
+        if ch.is_whitespace() || ch == '\n' { continue; }
         match ch {
             '(' => tokens.push(Token::LP),
             ')' => tokens.push(Token::RP),
@@ -41,10 +50,125 @@ pub fn lexical_analysis(s: &str) -> Vec<Token> {
             '}' => tokens.push(Token::RC),
             '[' => tokens.push(Token::LB),
             ']' => tokens.push(Token::RB),
-            ';' => tokens.push(Token::RB),
+            ';' => tokens.push(Token::Semicolon),
             ',' => tokens.push(Token::Comma),
             '.' => tokens.push(Token::Dot),
+            '>' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::BiggerOrEqual);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Bigger),
+            },
+            '<' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::LesserOrEqual);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Lesser),
+            },
+            '=' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::Equal);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Assignment),
+            },
+            '!' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::NotEqual);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Negation),
+            },
+            '+' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::PlusEqual);
+                    chr.next();
+                },
+                Some(&'+') => {
+                    tokens.push(Token::Increment);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Plus),
+            },
+            '-' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::MinusEqual);
+                    chr.next();
+                },
+                Some(&'-') => {
+                    tokens.push(Token::Decrement);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Minus),
+            },
+            '*' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::StarEqual);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Star),
+            },
+            '/' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::DivEqual);
+                    chr.next();
+                },
+                Some(&'/') => {
+                    while Some(&'\n') != chr.peek() {
+                        chr.next();
+                    }
+                    chr.next();
+                },
+                _ => tokens.push(Token::Div),
+            },
+            '%' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::ModEqual);
+                    chr.next();
+                },
+                _ => tokens.push(Token::Mod),
+            },
+            '&' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::BitwiseAndEqual);
+                    chr.next();
+                },
+                Some(&'&') => {
+                    tokens.push(Token::And);
+                    chr.next();
+                },
+                _ => tokens.push(Token::BitwiseAnd),
+            },
+            '|' => match chr.peek() {
+                Some(&'=') => {
+                    tokens.push(Token::BitwiseOrEqual);
+                    chr.next();
+                },
+                Some(&'|') => {
+                    tokens.push(Token::Or);
+                    chr.next();
+                },
+                _ => tokens.push(Token::BitwiseOr),
+            },
+            '\'' => {
+                let temp_ch;
+                match chr.next() {
+                    Some('\\') => match chr.next() {
+                        Some('\'') => panic!(),
+                        Some(x) => temp_ch = x,
+                        _ => panic!()
+                    },
+                    Some(x) => temp_ch = x,
+                    _ => panic!()
+                }
 
+                match chr.next() {
+                    Some('\'') => tokens.push(Token::CharLit { c: temp_ch as u8 }),
+                    _ => panic!()
+                }
+            },
             _ if ch.is_alphabetic() || ch == '_' => {
                 let mut lex = String::new();
                 lex.push(ch);
@@ -64,11 +188,8 @@ pub fn lexical_analysis(s: &str) -> Vec<Token> {
                     "break" => tokens.push(Token::Break),
                     "continue" => tokens.push(Token::Continue),
                     "return" => tokens.push(Token::Return),
-                    // "long" => tokens.push(Token::LongT),
                     "int" => tokens.push(Token::IntT),
-                    // "short" => tokens.push(Token::ShortT),
                     "double" => tokens.push(Token::DoubleT),
-                    // "float" => tokens.push(Token::FloatT),
                     "char" => tokens.push(Token::CharT),
                     "void" => tokens.push(Token::VoidT),
                     "true" => tokens.push(Token::True),
